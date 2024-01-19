eadd3bf02aad1f9d60cc2b5b9d766428ef6d80cb
Alicia-74 <aliciahernandezcontreras7@gmail.com>
2024-01-19 14:08:22 +0100

    Login de Google

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleLogin
+    onSuccess={credentialResponse => {
+      const credentialResponseCoded = jwtDecode(credentialResponse.credential);
+      console.log(credentialResponseCoded);
+    }}
+    onError={() => {
+      console.log('Login Failed');
+    }}
+  />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="968784867247-5uvsi1m7rvncg79a0tj38l8itrhvm2pi.apps.googleusercontent.com">
+        <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
